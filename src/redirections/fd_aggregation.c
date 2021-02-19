eb5d41453f57cdffa04b54a2561b8454bfc8cf8d
Youssef Lagtab <yousseflagtab@gmail.com>
2021-02-19 17:49:07 +0100

    change protoype

--- src/redirections/fd_aggregation.c
+++ src/redirections/fd_aggregation.c
@@ -6,13 +6,13 @@
 /*   By: ylagtab <ylagtab@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/19 08:19:42 by ylagtab           #+#    #+#             */
-/*   Updated: 2021/02/19 09:39:22 by ylagtab          ###   ########.fr       */
+/*   Updated: 2021/02/19 15:51:22 by ylagtab          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "internal.h"
 
-void	fd_aggregation(int new_fd, int old_fd)
+void	fd_aggregation(int old_fd, int new_fd)
 {
 	if (dup2(old_fd, new_fd) == -1)
 	{
