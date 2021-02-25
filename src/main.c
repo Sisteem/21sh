81e5f267aa536892280dc1c3d676cb3dd3364659
Youssef Lagtab <yousseflagtab@gmail.com>
2021-02-25 12:34:48 +0100

    add shell_main

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: ylagtab <ylagtab@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/08 09:47:37 by ylagtab           #+#    #+#             */
-/*   Updated: 2021/02/22 18:05:27 by ylagtab          ###   ########.fr       */
+/*   Updated: 2021/02/25 12:25:40 by ylagtab          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,6 +31,12 @@ const char	*g_tokens_keys[] = {
 	"GREATAND",
 	"GREATANDDASH",
 };
+
+void			handler(int unused)
+{
+	(void)unused;
+}
+
 
 void		print_tokens(t_vector *tokens)
 {
@@ -59,6 +65,11 @@ char		*read_cmd(void)
 	ft_printf(1, "$ ");
 	if (get_next_line(0, &cmd) < 0)
 		exit(1);
+	if (cmd == NULL)
+	{
+		ft_printf(1, "exit\n");
+		exit(0);
+	}
 	if (*cmd == '\0')
 		return (cmd);
 	tmp = cmd;
@@ -67,16 +78,60 @@ char		*read_cmd(void)
 	return (cmd);
 }
 
-int			main(int ac, char *av[], char *envp[])
+static	void	free_mini_env(t_vector *mini_env)
+{
+	size_t		i;
+
+	i = 0;
+	while (i < mini_env->length)
+	{
+		free_env_var(mini_env->array[i]->content, 0);
+		free(mini_env->array[i]);
+		++i;
+	}
+	free(mini_env->array);
+	mini_env = NULL;
+}
+
+static int		shell_main(char *arg_cmd)
 {
 	t_vector	*commands;
 	char		*line;
+	int			ret_status;
 
+
+	while (1337)
+	{
+		g_errno = EXIT_SUCCESS;
+		line = arg_cmd ? ft_strdup(arg_cmd) : read_cmd();
+		commands = parse_command(line);
+		ft_strdel(&line);
+		if (commands != NULL)
+		{
+			expansion(commands);
+			ret_status = exec_commands(commands);
+			g_errno = ret_status;
+		}
+		if (arg_cmd != NULL)
+			return (g_errno);
+	}
+	return (0);
+}
+
+int				main(int ac, char *av[], char *envp[])
+{
+	t_vector	mini_env;
+	int			exit_value;
+
+	exit_value = 1;
+	signal(SIGINT, &handler);
 	g_shell_env = env_init(envp);
+	if (ac == 3 && ft_strcmp(av[1], "-c") == 0)
+		exit_value = shell_main(av[2]);
 	if (ac == 1)
-		line = read_cmd();
-	else
-		line = av[ac - 1];
-	commands = parse_command(line);
-	return (exec_commands(commands));
+		exit_value = shell_main(NULL);
+	if (exit_value != 0)
+		ft_printf(2, "Error\nusage: ./21sh [-c command]\n");
+	free_mini_env(&mini_env);
+	return (exit_value);
 }
