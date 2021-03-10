dd6d3838362b41c5102f1b93cf22037fd9e46426
Youssef Lagtab <yousseflagtab@gmail.com>
2021-03-10 15:00:21 +0100

    close all pipes fds in childs and parent process. fix #12

--- src/execution/exec_pipe_sequence.c
+++ src/execution/exec_pipe_sequence.c
@@ -6,91 +6,104 @@
 /*   By: ylagtab <ylagtab@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/21 09:27:29 by ylagtab           #+#    #+#             */
-/*   Updated: 2021/03/10 09:45:28 by ylagtab          ###   ########.fr       */
+/*   Updated: 2021/03/10 14:55:47 by ylagtab          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "internal.h"
 
-static int	**create_pipes(size_t count)
+static int	**create_fds(size_t count)
 {
-	int		**pipes;
+	int		**fds;
 	size_t	i;
 
 	i = 0;
-	pipes = (int**)ft_malloc(count * sizeof(int*));
+	fds = (int**)ft_malloc(count * sizeof(int*));
 	while (i < count)
 	{
-		pipes[i] = (int*)ft_malloc(2 * sizeof(int));
-		if (pipe(pipes[i]) == -1)
+		fds[i] = (int*)ft_malloc(2 * sizeof(int));
+		if (pipe(fds[i]) == -1)
 		{
 			g_errno = EUNK;
 			ft_perror(NULL, NULL, FALSE);
 		}
 		++i;
 	}
-	return (pipes);
+	return (fds);
 }
 
-static void	free_pipes_array(int **pipes, size_t count)
+static void	close_fds(int **fds, size_t count)
 {
 	size_t	i;
 
 	i = 0;
 	while (i < count)
 	{
-		close(pipes[i][0]);
-		close(pipes[i][1]);
-		ft_memdel((void**)&(pipes[i]));
+		close(fds[i][0]);
+		close(fds[i][1]);
 		++i;
 	}
-	free(pipes);
 }
 
-static int	exec_pipe_command(t_command *cmd, t_command_fds *cmd_fds)
+static void	free_fds_array(int **fds, size_t count)
+{
+	size_t	i;
+
+	i = 0;
+	while (i < count)
+	{
+		close(fds[i][0]);
+		close(fds[i][1]);
+		ft_memdel((void**)&(fds[i]));
+		++i;
+	}
+	free(fds);
+}
+
+static int	exec_pipe_command(
+	t_command *cmd, size_t index, int **fds, size_t fds_count)
 {
 	int exit_status;
 
-	if (cmd_fds->in != -1)
-		if (dup2(cmd_fds->in, STDIN_FILENO) == -1)
+	if (index > 0)
+		if (dup2(fds[index - 1][0], STDIN_FILENO) == -1)
 		{
 			g_errno = EREDIRECTION;
 			ft_perror(NULL, NULL, TRUE);
 		}
-	if (cmd_fds->out != -1)
-		if (dup2(cmd_fds->out, STDOUT_FILENO) == -1)
+	if (index < fds_count)
+		if (dup2(fds[index][1], STDOUT_FILENO) == -1)
 		{
 			g_errno = EREDIRECTION;
 			ft_perror(NULL, NULL, TRUE);
 		}
+	close_fds(fds, fds_count);
 	exit_status = exec_simple_command(cmd->tokens, FALSE);
 	exit(exit_status);
 }
 
 int			exec_pipe_sequence(t_vector *tokens)
 {
-	t_command_fds	cmd_fds;
 	t_vector		commands;
-	int				**pipes;
+	int				**fds;
 	int				exit_status;
+	size_t			fds_count;
 	size_t			i;
 
 	split_pipe_sequence_commands(tokens, &commands);
-	pipes = create_pipes(commands.length - 1);
+	fds_count = commands.length - 1;
+	fds = create_fds(fds_count);
 	i = 0;
 	while (i < commands.length)
 	{
-		cmd_fds.in = i == 0 ? -1 : pipes[i - 1][0];
-		cmd_fds.out = i == commands.length - 1 ? -1 : pipes[i][1];
 		if (fork() == 0)
-			return (exec_pipe_command(commands.array[i]->content, &cmd_fds));
-		if (cmd_fds.out != -1)
-			close(cmd_fds.out);
+			exec_pipe_command(commands.array[i]->content, i, fds, fds_count);
 		++i;
 	}
+	close_fds(fds, fds_count);
 	while (wait(&exit_status) != -1)
 		;
-	free_pipes_array(pipes, commands.length - 1);
+	free_fds_array(fds, fds_count);
 	ft_vector_free(&commands, FALSE, del_command_without_tokens);
 	return (exit_status);
 }
